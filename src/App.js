b9fdfe8170d48d187465ce6632e3360d8d635e98
wangpengpeng <wangpengpeng@ttjianbao.com>
2021-01-25 16:04:52 +0800

    修改图标,修改错别字

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import { list } from './data.json'
 
 const read_time_key = 'read_time_key'
 const record_index_key = 'record_index_key'
+const pageSize = 30
+const pageCount = Math.ceil(list.length / pageSize)
 function App() {
 	const index = +(localStorage.getItem(record_index_key) || 0)
 	const [ info, setInfo ] = useState({ ...list[index] })
-
+	const [ showText, setShowText ] = useState(true)
+	const [ pageIndex, setPageIndex ] = useState(0)
 	useEffect(() => {
 		const time = localStorage.getItem(read_time_key)
 		const nowDate = new Date(new Date().toLocaleDateString()).getTime() // 获取当天凌晨的时间戳
@@ -23,11 +26,43 @@ function App() {
 		}
 	}, [])
 	const onDoubleClick = () => {
-		localStorage.setItem(record_index_key, '4')
+		setShowText(!showText)
 	}
+	const onNextPage = (e) => {
+		e.stopPropagation()
+		setPageIndex(pageIndex + 1)
+	}
+	const onPrePage = (e) => {
+		e.stopPropagation()
+		setPageIndex(pageIndex - 1)
+	}
+
 	return (
-		<div className="App" onDoubleClick={onDoubleClick}>
-			<section className="content">{info.text}</section>
+		<div className="container">
+			{showText ? (
+				<div className="App">
+					<section className="content">{info.text}</section>
+				</div>
+			) : (
+				<div className="list-block">
+					{list
+						.slice(20 * pageIndex, (pageIndex + 1) * pageSize)
+						.map((item, index) => (
+							<section key={item.text} className="row">{`${index +
+								1 +
+								pageIndex * pageSize}.${item.text}`}</section>
+						))}
+					<section className="footer">
+						<div className="btn-page" onClick={onPrePage}>
+							上一页
+						</div>
+						<div className="page-text">{`${pageIndex + 1}/${pageCount}`}</div>
+						<div className="btn-page" onClick={onNextPage}>
+							下一页
+						</div>
+					</section>
+				</div>
+			)}
 		</div>
 	)
 }
