719f83c3335773dd53e7232fb365a2b131f1238a
code先生 <wangpeng126_2009@126.com>
2021-05-20 17:21:36 +0800

    增加索引

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import { list } from './data.json'
+import Dialog from "./Dialog";
 
 const read_time_key = 'read_time_key'
 const record_index_key = 'record_index_key'
 const pageSize = 30
+const len = list.length;
 const pageCount = Math.ceil(list.length / pageSize)
 function App() {
 	const params = new URL(document.location).searchParams
@@ -13,9 +15,10 @@ function App() {
 		localStorage.setItem(record_index_key, initIndex)
 	}
 	const index = +(localStorage.getItem(record_index_key) || 0)
-	const [ info, setInfo ] = useState({ ...list[index] })
-	const [ showText, setShowText ] = useState(true)
-	const [ pageIndex, setPageIndex ] = useState(0)
+	const [info, setInfo] = useState({ ...list[index] })
+	const [showText, setShowText] = useState(true)
+	const [pageIndex, setPageIndex] = useState(0)
+	const [visibleVideo, setVisibleVideo] = useState(false)
 	useEffect(() => {
 		const time = localStorage.getItem(read_time_key)
 		const nowDate = new Date(new Date().toLocaleDateString()).getTime() // 获取当天凌晨的时间戳
@@ -41,33 +44,42 @@ function App() {
 		e.stopPropagation()
 		setPageIndex(pageIndex - 1)
 	}
+	const onShowVideo = () => {
+		setVisibleVideo(true)
+	}
+	const onCloseDialog = () => {
+		setVisibleVideo(false)
+	}
 
 	return (
 		<div className="container">
 			{showText ? (
 				<div className="App">
 					<section className="content">{info.text}</section>
+					<section className="index" onClick={onShowVideo}>{index}</section>
+					<section className="length" >{len}</section>
+					<Dialog visible={visibleVideo} onCloseDialog={onCloseDialog} />
 				</div>
 			) : (
-				<div className="list-block">
-					{list
-						.slice(20 * pageIndex, (pageIndex + 1) * pageSize)
-						.map((item, index) => (
-							<section key={item.text} className="row">{`${index +
-								1 +
-								pageIndex * pageSize}.${item.text}`}</section>
-						))}
-					<section className="footer">
-						<div className="btn-page" onClick={onPrePage}>
-							上一页
+					<div className="list-block">
+						{list
+							.slice(20 * pageIndex, (pageIndex + 1) * pageSize)
+							.map((item, index) => (
+								<section key={item.text} className="row">{`${index +
+									1 +
+									pageIndex * pageSize}.${item.text}`}</section>
+							))}
+						<section className="footer">
+							<div className="btn-page" onClick={onPrePage}>
+								上一页
 						</div>
-						<div className="page-text">{`${pageIndex + 1}/${pageCount}`}</div>
-						<div className="btn-page" onClick={onNextPage}>
-							下一页
+							<div className="page-text">{`${pageIndex + 1}/${pageCount}`}</div>
+							<div className="btn-page" onClick={onNextPage}>
+								下一页
 						</div>
-					</section>
-				</div>
-			)}
+						</section>
+					</div>
+				)}
 		</div>
 	)
 }
---
+++ src/Dialog.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import './Dialog.css'
+
+const record_index_key = 'record_index_key'
+export default function Dialog(props) {
+    const { visible, onCloseDialog } = props
+    const [index, setIndex] = useState(localStorage.getItem(record_index_key) || '0');
+    const onChangeIndex = function (e) {
+        setIndex(e.target.value);
+    }
+    const onSubmit = () => {
+        localStorage.setItem(record_index_key, index);
+        onCloseDialog();
+    }
+    return <div className="dialog-container" style={{ visibility: visible ? 'visible' : 'hidden' }}>
+        <section className="dialog-content">
+            <img src="http://img.wangpengpeng.site/close.svg" alt="关闭" className="dialog-close" onClick={onCloseDialog} />
+            <input className="input-text" value={index} onChange={onChangeIndex} />
+            <input type="button" value="确定" className="btn-ok" onClick={onSubmit} />
+        </section>
+    </div>
+}
